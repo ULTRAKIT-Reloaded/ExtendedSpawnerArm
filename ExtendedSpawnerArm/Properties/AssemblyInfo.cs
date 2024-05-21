34f3350bfdf57ba2b6348841f13ad6586bce0bfe
PetersonE1 <PetersonE_1@live.com>
2024-05-20 23:11:31 -0400

    organization, name changes

--- ExtendedSpawnerArm/Properties/AssemblyInfo.cs
+++ ExtendedSpawnerArm/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExtendedSpawnerArm")]
+[assembly: AssemblyTitle("ULTRAKIT.ExtendedSpawnerArm")]
 [assembly: AssemblyDescription("BepInEx plugin to extend the spawner arm in ULTRAKILL.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ULTRAKIT")]
